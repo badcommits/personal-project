bd34b6426b9951e5292e0706532186d606a689fe
Harsh <harsh.jha@example.com>
2022-07-11 22:56:51 +0530

    added more data to commit

--- add-data.py
+++ add-data.py
@@ -6,5 +6,11 @@ client = MongoClient("mongodb://localhost:27017/")
 database = client["test_db"]
 collection = database["test_col"]
 
+# sample data
+data = [{"name": "Return of the Native", "author": "Thomas Hardy"}, {"name": "Dracula", "author": "Bram Stoker"}]
+
+# insert data
+inserted_data = collection.insert_many(data)
+
 all_data = list(collection.find())
 print(json.dumps(all_data, default=json_util.default))
